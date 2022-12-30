ff571ddabf5c1c25f10ce1d4f517fdc741792b5e
pranshu <pranshugautam1998@gmail.com>
2022-12-30 13:10:54 +0530

    commit 14

--- src/main/java/com/example/library/studentlibrary/controller/TransactionController.java
+++ src/main/java/com/example/library/studentlibrary/controller/TransactionController.java
@@ -16,17 +16,17 @@ public class TransactionController {
     TransactionService ts;
 
     //Add required annotations
-//    @PostMapping("/issueBook")
-//    public ResponseEntity<String> issueBook(@RequestParam("cardId") int cardId, @RequestParam("bookId") int bookId) throws Exception{
-//        String transId = ts.issueBook(cardId , bookId);
-//       //return new ResponseEntity<>("transaction completed", HttpStatus.ACCEPTED);
-//        return new ResponseEntity<>(transId , HttpStatus.ACCEPTED);
-//    }
-//
-//    //Add required annotations
-//    @PostMapping("/returnBook")
-//    public ResponseEntity<String> returnBook(@RequestParam("cardId") int cardId, @RequestParam("bookId") int bookId) throws Exception{
-//        ts.returnBook(cardId , bookId);
-//        return new ResponseEntity<>("transaction completed", HttpStatus.ACCEPTED);
-//    }
+    @PostMapping("/issueBook")
+    public ResponseEntity<String> issueBook(@RequestParam("cardId") int cardId, @RequestParam("bookId") int bookId) throws Exception{
+        String transId = ts.issueBook(cardId , bookId);
+       //return new ResponseEntity<>("transaction completed", HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(transId , HttpStatus.ACCEPTED);
+    }
+
+    //Add required annotations
+    @PostMapping("/returnBook")
+    public ResponseEntity<String> returnBook(@RequestParam("cardId") int cardId, @RequestParam("bookId") int bookId) throws Exception{
+        ts.returnBook(cardId , bookId);
+        return new ResponseEntity<>("transaction completed", HttpStatus.ACCEPTED);
+    }
 }
